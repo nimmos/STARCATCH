16b42be151bcba00abbbe3cafca2cb1852db018c
NII <nisanvera23@gmail.com>
2015-01-24 01:32:11 +0100

    Añadidas las características de idols y una clase auxiliar que aún no sirve para nada.

---
+++ src/characters/Auxiliar.java
@@ -0,0 +1,14 @@
+package characters;
+
+/**
+ * STARCATCH!! - Auxiliar
+ * Method set for auxiliar operations performed
+ *  on things related to Idol class
+ * 
+ * @author "Daniel Sánchez" <nisanvera23@gmail.com>
+ */
+public class Auxiliar {
+    
+    
+    
+}
--- src/characters/Idol.java
+++ src/characters/Idol.java
@@ -1,12 +1,18 @@
 package characters;
 
 /**
- * STARCATCH!!
- * Idols, idols and more idols, what else would you want?
+ * STARCATCH!! - Idol
+ * Main class for a girly idol wannabe.
+ *  May contain: a lot of unused characteristics
  * 
  * @author "Daniel Sánchez" <nisanvera23@gmail.com>
  */
 public class Idol {
+    
+    /*
+        CLASS CONSTANTS
+    */
+    private final static double CHAR_MAX = 100.0;
     
     /*
         PRIVATE ATTRIBUTES
@@ -23,6 +29,21 @@ public class Idol {
     
     // Characteristics
     private int level;
+    // SMART-ASS characteristics
+    private double SMA;
+    private double sin;
+    private double dan;
+    private double shp;
+    // CHARISMA characteristics
+    private double CHA;
+    private double lov;
+    private double atr;
+    private double ero;
+    // PURRRITY characteristics
+    private double PUR;
+    private double aur;
+    private double hrt;
+    private double spk;
     
     /*
         CONSTRUCTORS
@@ -40,6 +61,21 @@ public class Idol {
     public String getStatus () { return this.status; }
     
     public int getLevel () { return this.level; }
+    
+    public double getSMA () { return SMA; }
+    public double getSin () { return sin; }
+    public double getDan () { return dan; }
+    public double getShp () { return shp; }
+
+    public double getCHA () { return CHA; }
+    public double getLov () { return lov; }
+    public double getAtr () { return atr; }
+    public double getEro () { return ero; }
+
+    public double getPUR () { return PUR; }
+    public double getAur () { return aur; }
+    public double getHrt () { return hrt; }
+    public double getSpk () { return spk; }
 
     /*
         SET METHODS
@@ -53,5 +89,20 @@ public class Idol {
     public void setStatus (String status) { this.status = status; }
     
     public void setLevel (int level) { this.level = level; }
-    
+
+    public void setSMA (double SMA) { this.SMA = SMA; }
+    public void setSin (double sin) { this.sin = sin; }
+    public void setDan (double dan) { this.dan = dan; }
+    public void setShp (double shp) { this.shp = shp; }
+
+    public void setCHA (double CHA) { this.CHA = CHA; }
+    public void setLov (double lov) { this.lov = lov; }
+    public void setAtr (double atr) { this.atr = atr; }
+    public void setEro (double ero) { this.ero = ero; }
+
+    public void setPUR (double PUR) { this.PUR = PUR; }
+    public void setAur (double aur) { this.aur = aur; }
+    public void setHrt (double hrt) { this.hrt = hrt; }
+    public void setSpk (double spk) { this.spk = spk; }
+
 }
